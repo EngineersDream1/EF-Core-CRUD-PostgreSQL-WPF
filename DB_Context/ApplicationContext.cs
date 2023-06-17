7df43a2bb2f03e93f05b50ce040b4f8224bd54be
Mihail Goncharov <m.goncharov@gmail.com>
2023-06-18 00:23:31 +0300

    Добавлены класс контекста (ApplicationContext) и класс модели (User). В модели начата реализация интерфейса INotifyPropertyChanged.

---
+++ DB_Context/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Entity_Framework_WPF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_WPF.DB_Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=admin");
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_WPF.Model
+{
+    public class User : INotifyPropertyChanged
+    {        
+        private string name;
+        private string email;
+
+        public int ID { get; set; }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                value = name;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                value = email;
+                OnPropertyChanged("Email");
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop ="")
+        {
+            if(PropertyChanged != null)            
+               PropertyChanged(this, new PropertyChangedEventArgs(prop));            
+        }
+    }
+}
