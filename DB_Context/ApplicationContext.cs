5e7b4747753dbb5a909ed02981610eab927b1485
Михаил <@DESKTOP-1120MHP>
2023-06-22 22:24:07 +0300

    В классе ApplicationContext реализованно логирование c записью в фаил logger.txt через StreamWriter.

--- DB_Context/ApplicationContext.cs
+++ DB_Context/ApplicationContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace Entity_Framework_WPF.DB_Context
     {
         public DbSet<User> Users { get; set; }
 
+        readonly StreamWriter logStream = new StreamWriter("logger.txt", true);
+
         public ApplicationContext()
         {
             Database.EnsureCreated();
@@ -20,6 +23,19 @@ namespace Entity_Framework_WPF.DB_Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=admin");
+            optionsBuilder.LogTo(logStream.WriteLine);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            logStream.Dispose();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logStream.DisposeAsync();
         }
     }
 }
